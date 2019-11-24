b848cd6e726b5c765607f22cfd35af64ec8b6320
huangpeng <huangpeng@sensorsdata.cn>
2019-11-24 21:07:41 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/data_structure_study_.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/data_structure_study_.dir/main.cpp.o"
+  "data_structure_study_"
+  "data_structure_study_.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/data_structure_study_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
